a624909c686760e80f05e8511145153d6f12312f
Ken Udoudo <kendo302003@gmail.com>
2024-05-09 07:10:46 -0700

    text

--- src/components/section2.jsx
+++ src/components/section2.jsx
@@ -1,6 +1,13 @@
 const Sec2 = () => {
     return (  
-        
+        <div>
+            <div>
+                <p>ABOUT US</p>
+                <p>Agency in the field of <br />Photography</p>
+                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Consectetur fugit tempore <br /> aut ea inventore optio rerum corrupti, quia impedit, voluptatem doloribus exercitationem <br /> et nihil esse possimus! Velit consequatur eveniet voluptate.</p>
+                <p>See More</p>
+            </div>
+        </div>
     );
 }
  
