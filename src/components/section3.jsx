cbd9a9540b004965bb73930027c270615126d180
Ken Udoudo <kendo302003@gmail.com>
2024-05-09 23:43:51 -0700

    mt

--- src/components/section3.jsx
+++ src/components/section3.jsx
@@ -1,7 +1,7 @@
 const Sec3 = () => {
     return (  
-        <div>
-            <div>
+        <div className="px-[20px] mt-[100px]">
+            <div className="flex flex-col justify-start items-start">
                 <p className='text-[13px] text-[#797878] border-[2px] px-[8px] py-[2px] rounded-full'>SERVICES</p>
                 <p className='text-[40px] font-bold'>We provide various best services to<br />transform your ideas</p>
             </div>
