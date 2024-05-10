65350dc5996f1e12f558c9b2f2c42bafa108b2e3
Ken Udoudo <kendo302003@gmail.com>
2024-05-09 23:42:05 -0700

    sec3

---
+++ src/components/section3.jsx
@@ -0,0 +1,12 @@
+const Sec3 = () => {
+    return (  
+        <div>
+            <div>
+                <p className='text-[13px] text-[#797878] border-[2px] px-[8px] py-[2px] rounded-full'>SERVICES</p>
+                <p className='text-[40px] font-bold'>We provide various best services to<br />transform your ideas</p>
+            </div>
+        </div>
+    );
+}
+ 
+export default Sec3;
