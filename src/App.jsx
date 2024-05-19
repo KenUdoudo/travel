0ac359d9bc5e457ea5c3a2a60b01e1fef165f5f3
Ken Udoudo <kendo302003@gmail.com>
2024-05-19 01:49:48 -0700

    importing sec4

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import Sec1 from "./components/section1"
 import Sec2 from "./components/section2"
 import Sec3 from "./components/section3"
+import Sec4 from "./components/section4"
 
 
 
@@ -14,6 +15,8 @@ function App() {
       <Sec2 />
 
       <Sec3 />
+
+      <Sec4 />
     </div>
   )
 }
---
+++ src/components/section4.jsx
@@ -0,0 +1,12 @@
+const Sec4 = () => {
+    return (  
+        <div className="px-[20px] mt-[100px]">
+            <div className="flex flex-col justify-start items-start">
+                <p className='text-[13px] text-[#797878] border-[2px] px-[8px] py-[2px] rounded-full'>OUR PROJECT</p>
+                <p className='text-[40px] font-bold'>Explore our projects, where each<br />project reflects innovation</p>
+            </div>
+        </div>
+    );
+}
+ 
+export default Sec4;
