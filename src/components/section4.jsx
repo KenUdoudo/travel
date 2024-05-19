6fac51c373703d0399ca99df8287fad7ec7ecb4c
Ken Udoudo <kendo302003@gmail.com>
2024-05-19 02:17:33 -0700

    first img

--- src/components/section4.jsx
+++ src/components/section4.jsx
@@ -1,9 +1,17 @@
+import nine from '../assets/9.jpg';
 const Sec4 = () => {
     return (  
         <div className="px-[20px] mt-[100px]">
             <div className="flex flex-col justify-start items-start">
                 <p className='text-[13px] text-[#797878] border-[2px] px-[8px] py-[2px] rounded-full'>OUR PROJECT</p>
                 <p className='text-[40px] font-bold'>Explore our projects, where each<br />project reflects innovation</p>
+            </div>
+            <div className="flex flex-col w-full relative">
+                <div className='w-[55%] flex flex-col items-end absolute right-0'>
+                    <img src={ nine } className='w-full h-[700px] object-cover'/>
+                    <p className='text-[30px] font-semibold'>NEON LIGHTS</p>
+                    <p className='text-[20px] text-[#797878]'>United States / Portait Photography</p>
+                </div>
             </div>
         </div>
     );
