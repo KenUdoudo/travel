c543f183a222c9c270c10459fab88eaeea3d8b3d
Ken Udoudo <kendo302003@gmail.com>
2024-05-19 02:37:47 -0700

    last one

--- src/components/section4.jsx
+++ src/components/section4.jsx
@@ -2,6 +2,7 @@ import nine from '../assets/9.jpg';
 import ten from '../assets/10.jpg';
 import eleven from '../assets/11.jpg';
 import twelve from '../assets/12.jpg';
+import thirteen from '../assets/13.jpg';
 const Sec4 = () => {
     return (  
         <div className="px-[20px] mt-[100px]">
@@ -29,6 +30,11 @@ const Sec4 = () => {
                     <img src={ twelve } className='w-full h-[500px] object-cover'/>
                     <p className='text-[30px] font-semibold'>HILL DRIVE</p>
                     <p className='text-[20px] text-[#797878]'>Swiss Alps / Urban Photography</p>
+                </div>
+                <div className='w-[40%] flex flex-col items-end absolute right-0 top-[1700px]'>
+                    <img src={ thirteen } className='w-full h-[500px] object-cover'/>
+                    <p className='text-[30px] font-semibold text-right'>Are these all projects? No, click to see other projects</p>
+                    <p className='bg-black text-white text-[13px] px-[15px] py-[5px] rounded-full mt-[10px]'>See More</p>
                 </div>
             </div>
             
